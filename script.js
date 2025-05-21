647f1c25bfbf43956a9f67d6e74387dd908065e2
dereckc210 <dereck.pirulin@icloud.com>
2025-05-21 12:37:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const addButtons = document.querySelectorAll("cart");
 
 const removeButton = document.getElementById("remove-btn");
 
+addButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const itemName = button.getAttribute("data-time");
+
+    const option = document.createElement("option");
+    option.textContent = itemName;
+    option.value = itemName;
+
+    cart.____________________;
+  });
+});
